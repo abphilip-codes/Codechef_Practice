d0f242cbc5f7d4f7836f07fdcbeeeca50265a056
Allen <allenalvin333@gmail.com>
2021-07-15 08:17:37 +0530

    programs

--- Beginner/1.py
+++ Beginner/1.py
@@ -1,5 +1,5 @@
 # https://www.codechef.com/problems/HS08TEST
 
 x,y = map(float,input().split())
-if(x%5==0 and x<y): print("{:.2f}".format(y-x-0.5))
+if(x%5==0 and (x+0.5)<=y): print("{:.2f}".format(y-x-0.5))
 else: print("{:.2f}".format(y))
