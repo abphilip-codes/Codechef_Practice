96753df365d6f4cf4867e3883a8907839765567b
Allen <allenalvin333@gmail.com>
2021-07-17 07:30:39 +0530

    programs

--- Beginner/13.py
+++ Beginner/13.py
@@ -1,2 +1,12 @@
 # https://www.codechef.com/problems/TLG
 
+p1=p2=al=0
+ans=""
+for T in range(int(input())): 
+    a,b = map(int,input().split(" "))
+    p1,p2 = p1+a,p2+b
+    if(abs(p1-p2)>=al):
+        al=abs(p1-p2)
+        if(p1>p2): ans="1 {}".format(p1-p2)
+        else: ans="2 {}".format(p2-p1)
+print(ans)
