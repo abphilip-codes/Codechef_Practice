22695a076320ec5c0ea977f736c371686fb3afb6
Allen <allenalvin333@gmail.com>
2021-07-18 08:58:03 +0530

    programs

--- Beginner/18.py
+++ Beginner/18.py
@@ -1,2 +1,7 @@
 # https://www.codechef.com/problems/CHOPRT
 
+for T in range(int(input())):
+    a,b = map(int,input().split())
+    if(a>b): print(">")
+    if(a<b): print("<")
+    if(a==b): print("=")
