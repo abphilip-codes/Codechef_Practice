a3c5ecca39c85c39557c44ec0386b74488d16afb
Allen <allenalvin333@gmail.com>
2021-07-19 06:38:47 +0530

    programs

--- Beginner/23.py
+++ Beginner/23.py
@@ -1,2 +1,5 @@
 # https://www.codechef.com/problems/AMR15A
 
+N = int(input())
+l = list(map(int,input().split()))
+print("READY FOR BATTLE") if len(list(filter(lambda x:x%2==0,l)))>len(list(filter(lambda x:x%2!=0,l))) else print("NOT READY")
