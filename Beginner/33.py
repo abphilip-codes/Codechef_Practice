78879b291ffdd8890c6a7d7bd933c8e40fb605fd
Allen <allenalvin333@gmail.com>
2021-07-20 11:55:51 +0530

    programs

--- Beginner/33.py
+++ Beginner/33.py
@@ -1,2 +1,5 @@
 # https://www.codechef.com/problems/FLOW011
 
+for T in range(int(input())):
+    n = int(input())
+    print("{:.2f}".format(n*2)) if(n<1500) else print("{:.2f}".format(n*1.98+500))
