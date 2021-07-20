42c1f5adf71741e47d0e33db21c945289a7f298b
Allen <allenalvin333@gmail.com>
2021-07-20 12:37:29 +0530

    programs

--- Beginner/32.py
+++ Beginner/32.py
@@ -1,2 +1,9 @@
 # https://www.codechef.com/problems/TRISQ
 
+def add(n):
+    s=0
+    for z in range(n+1): s+=z
+    return s
+for T in range(int(input())):
+    n = int(input())-3
+    print(add((n+1)//2))
