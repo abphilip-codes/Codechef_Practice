f8b52b0d04eb0fe4410660e1b967dabf6718a743
Allen <allenalvin333@gmail.com>
2021-07-21 07:36:54 +0530

    programs

--- Beginner/38.py
+++ Beginner/38.py
@@ -1,1 +1,11 @@
 # https://www.codechef.com/problems/RECIPE
+
+def check(z,l=[]):
+    for i in range(0,len(l)):
+        if(l[i]%z!=0): return False
+    return True
+for T in range(int(input())):
+    l = list(map(int,input().split()))[1:]
+    for z in range(min(l),1,-1):
+        if(check(z,l)==True): l[:] = [int(a/z) for a in l]
+    print(*l)
