9c5b2933e792b66cdad6ca92f4d9f8b1b82905c6
Allen <allenalvin333@gmail.com>
2021-07-21 08:43:41 +0530

    programs

--- Beginner/39.py
+++ Beginner/39.py
@@ -1,1 +1,9 @@
 # https://www.codechef.com/problems/PERMUT2
+
+while(int(input())):
+    l = list(map(int,input().split()))
+    for i in l:
+        if(l[i-1]!=(l.index(i)+1)): 
+            print("not ambiguous")
+            break
+    else: print("ambiguous")
