e230cd7e711a2dd5f35f5ee083137085ea00c5a9
Allen <allenalvin333@gmail.com>
2021-07-21 08:48:52 +0530

    programs

--- Beginner/40.py
+++ Beginner/40.py
@@ -1,1 +1,7 @@
 # https://www.codechef.com/problems/HEADBOB
+
+for T in range(int(input())):
+    n,s = int(input()),input()
+    if("I" in s): print("INDIAN")
+    elif(s.count("N")==len(s)): print("NOT SURE")
+    else: print("NOT INDIAN")
