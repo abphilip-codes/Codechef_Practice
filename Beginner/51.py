0a375b400032c0df20dfb203c910e84faacf3f9f
Allen <allenalvin333@gmail.com>
2021-07-23 14:02:10 +0530

    programs

--- Beginner/51.py
+++ Beginner/51.py
@@ -1,2 +1,9 @@
 # https://www.codechef.com/problems/TWTCLOSE
 
+n,t = map(int,input().split())
+l = [0]*n
+for T in range(t):
+    s = input().split()
+    if(s[0]=='CLOSEALL'): l = [0]*n
+    else: l[int(s[1])-1]=(l[int(s[1])-1]+1)%2
+    print(l.count(1))
