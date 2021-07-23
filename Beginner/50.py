b9f6a0b7c846005cb2225bac9b63e79d355ac3fe
Allen <allenalvin333@gmail.com>
2021-07-23 13:03:12 +0530

    programs

--- Beginner/50.py
+++ Beginner/50.py
@@ -1,4 +1,6 @@
 # https://www.codechef.com/problems/AREAPERI
 
 l,b=int(input()),int(input())
-print("Peri\n{}".format(2*(l+b))) if(2*(l+b)>l*b) else print("Area\n{}".format(l*b))
+if(2*(l+b)>l*b): print("Peri\n{}".format(2*(l+b)))  
+elif(2*(l+b)<l*b): print("Area\n{}".format(l*b))
+else: print("Eq\n{}".format(l*b))
