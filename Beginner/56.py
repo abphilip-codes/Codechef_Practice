a493516a979117be0dde4c5bf04ed616f6e5b578
allenalvin333 <allenalvin333@gmail.com>
2021-07-24 07:40:53 +0530

    programs

--- Beginner/56.py
+++ Beginner/56.py
@@ -1,12 +1,15 @@
 # https://www.codechef.com/problems/MNMX
 
 for T in range(int(input())):
-    n,a,s=int(input()),list(map(int,input().split())),0
-    for z in range(n-1): 
-        if(a[0]>=a[1]):
-            s+=a[1]
-            a.pop(0) 
-        else:
-            s+=a[0]
-            a.pop(1)
-    print(s)
+    n,a=int(input()),list(map(int,input().split()))
+    print((n-1)*min(a))
+    # If order matters 
+    # s=0 
+    # for z in range(n-1): 
+    #     if(a[0]>=a[1]):
+    #         s+=a[1]
+    #         a.pop(0) 
+    #     else:
+    #         s+=a[0]
+    #         a.pop(1)
+    # print(s)
