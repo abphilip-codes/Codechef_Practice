c28e3a6ac4d775747206c7b3b7c663583f04d1b4
allenalvin333 <allenalvin333@gmail.com>
2021-07-24 06:38:43 +0530

    programs

--- Beginner/56.py
+++ Beginner/56.py
@@ -1,2 +1,12 @@
 # https://www.codechef.com/problems/MNMX
 
+for T in range(int(input())):
+    n,a,s=int(input()),list(map(int,input().split())),0
+    for z in range(n-1): 
+        if(a[0]>=a[1]):
+            s+=a[1]
+            a.pop(0) 
+        else:
+            s+=a[0]
+            a.pop(1)
+    print(s)
