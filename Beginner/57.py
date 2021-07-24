f89990999bf0e0ddc453f599d4b61ec1888f6c16
allenalvin333 <allenalvin333@gmail.com>
2021-07-24 06:49:17 +0530

    programs

--- Beginner/57.py
+++ Beginner/57.py
@@ -1,2 +1,7 @@
 # https://www.codechef.com/problems/TWOSTR
 
+for T in range(int(input())):
+    a,b,s=input(),input(),0
+    for i in range(len(a)):
+        if(a[i]==b[i] or a[i]=="?" or b[i]=="?"): s+=1 
+    print("Yes") if(s==len(a)) else print("No")
