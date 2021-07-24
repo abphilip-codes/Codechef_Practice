3d488baa023e8e23299c0ad0466d67ec1e27aff8
allenalvin333 <allenalvin333@gmail.com>
2021-07-24 08:08:42 +0530

    programs

--- Beginner/55.py
+++ Beginner/55.py
@@ -1,2 +1,9 @@
 # https://www.codechef.com/problems/COPS
 
+for T in range(int(input())):
+    M,x,y=map(int,input().split())
+    m,a = list(map(int,input().split())),list(range(1,101))
+    for i in m:
+        for j in range(i-x*y,i+1+x*y):
+            if(j in a): a.remove(j)
+    print(len(a))
