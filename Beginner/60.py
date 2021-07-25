21598e8a3d0d0da89be3c1654422f1fbfa8b3832
allenalvin333 <allenalvin333@gmail.com>
2021-07-25 08:34:08 +0530

    programs

--- Beginner/60.py
+++ Beginner/60.py
@@ -1,2 +1,12 @@
 # https://www.codechef.com/problems/FRGTNLNG
 
+for T in range(int(input())):
+    N,K=map(int,input().split())
+    n,k=list(input().split()),[]
+    for j in range(K): k.append(list(input().split()))
+    for i in n:
+        mark="NO"
+        for j in range(K): 
+            if(i in k[j]): mark="YES"
+        print(mark,end=" ")
+    print()
