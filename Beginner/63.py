0df58c826749f2bd1fd4d6b45d01f92d90a23b12
allenalvin333 <allenalvin333@gmail.com>
2021-07-26 07:38:55 +0530

    programs

--- Beginner/63.py
+++ Beginner/63.py
@@ -1,1 +1,10 @@
 # https://www.codechef.com/problems/KTTABLE
+
+for T in range(int(input())):
+    n,count=int(input()),0
+    A=list(map(int,input().split()))
+    B=list(map(int,input().split()))
+    for i in range(1,n): A[i]=A[i]-sum(A[:i])
+    for i in range(n):
+        if(A[i]>=B[i]): count+=1 
+    print(count)
