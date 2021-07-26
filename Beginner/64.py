b27558253e81c8d33b72423566e4f5bcfed013f5
allenalvin333 <allenalvin333@gmail.com>
2021-07-26 07:44:33 +0530

    programs

--- Beginner/64.py
+++ Beginner/64.py
@@ -1,1 +1,12 @@
 # https://www.codechef.com/problems/ATM2
+
+for T in range(int(input())):
+    ret=""
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    for i in range(n):
+        if(a[i]<=k):
+            ret+="1"
+            k-=a[i]
+        else: ret+="0"
+    print(ret)
