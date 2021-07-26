c8feecbcb68a85ae3b39c94437733ca73164fae0
allenalvin333 <allenalvin333@gmail.com>
2021-07-26 07:48:52 +0530

    programs

--- Beginner/65.py
+++ Beginner/65.py
@@ -1,2 +1,9 @@
 # https://www.codechef.com/problems/RECTSQ
 
+def gcd(a,b):
+    while(b): a,b=b,a%b
+    return a
+for T in range(int(input())):
+    a,b=map(int,input().split())
+    c=gcd(a,b)
+    print(int((a*b)/(c*c)))
