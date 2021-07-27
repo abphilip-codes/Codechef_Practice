3e0574ecea4485a834a95ab77bbcd5e8e2449647
allenalvin333 <allenalvin333@gmail.com>
2021-07-27 08:08:15 +0530

    programs

--- Beginner/ALTARAY.py
+++ Beginner/ALTARAY.py
@@ -1,4 +1,9 @@
 # https://www.codechef.com/problems/ALTARAY
 
-s=input()
-for T in range(int(input())): print("Yes") if(set(input()).issubset(set(s))) else print("No")
+for T in range(int(input())):
+    n,l,c,s=int(input()),list(map(int,input().split())),1,""
+    for i in range(n-1,0,-1):
+        if(l[i]^l[i-1]<0): s,c=s+str(c)+" ",c+1
+        else: s,c=s+str(c)+" ",1
+    s+=str(c)
+    print(s[::-1])
