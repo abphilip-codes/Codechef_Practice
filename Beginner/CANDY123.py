99bc6ab737d7b7012d1a4119cb7c72d994ecf1a7
allenalvin333 <allenalvin333@gmail.com>
2021-07-27 07:39:54 +0530

    programs

--- Beginner/CANDY123.py
+++ Beginner/CANDY123.py
@@ -1,1 +1,6 @@
 # https://www.codechef.com/problems/CANDY123
+
+for T in range(int(input())):
+    l,n=list(map(int,input().split())),1
+    while((n**2)<=l[0] and (n*(n+1))<=l[1]): n+=1 
+    print("Bob") if((n*n)>l[0]) else print("Limak")
