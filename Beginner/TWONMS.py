097f09881049d235add02f6a150e73dfc740d4d2
allenalvin333 <allenalvin333@gmail.com>
2021-07-27 06:56:06 +0530

    programs

--- Beginner/TWONMS.py
+++ Beginner/TWONMS.py
@@ -1,1 +1,5 @@
 # https://www.codechef.com/problems/TWONMS
+
+for T in range(int(input())):
+    a,b,n=map(int,input().split())
+    print(max(a*(2**(n+1)),b*(2**n))//min(a*(2**(n+1)),b*(2**n))) if(n%2!=0) else print(max(a*(2**n),b*(2**n))//min(a*(2**n),b*(2**n)))
