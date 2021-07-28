e12d67be4110a730c540174912e523dbce5c5e16
allenalvin333 <allenalvin333@gmail.com>
2021-07-28 07:22:21 +0530

    programs

--- Beginner/RAINBOWA.py
+++ Beginner/RAINBOWA.py
@@ -1,1 +1,5 @@
 # https://www.codechef.com/problems/RAINBOWA
+
+for T in range(int(input())):
+    n,l=int(input()),list(map(int,input().split()))
+    print("no") if(set(l)!=set(list(range(1,8))) or l[0]!=1 or l[-1]!=1 or l!=l[::-1]) else print("yes")
