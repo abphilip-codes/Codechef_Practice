c3ca9e3d1f5fb071719d1d098474606b85d6e510
allenalvin333 <allenalvin333@gmail.com>
2021-07-29 08:16:13 +0530

    programs

--- Beginner/VCS.py
+++ Beginner/VCS.py
@@ -3,4 +3,4 @@
 for T in range(int(input())):
     N,M,K=map(int,input().split())
     n,m,k=set(list(range(1,N+1))),set(list(map(int,input().split()))),set(list(map(int,input().split())))
-    print("{} {}".format(m&k,n-(m|k)))
+    print("{} {}".format(len(m&k),len(n-(m|k))))
