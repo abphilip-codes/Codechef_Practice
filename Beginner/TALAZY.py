a7c2cd88d98fc0700d1622fcaaa727c7eb4e34a6
allenalvin333 <allenalvin333@gmail.com>
2021-07-31 06:31:49 +0530

    programs

--- Beginner/TALAZY.py
+++ Beginner/TALAZY.py
@@ -1,1 +1,9 @@
 # https://www.codechef.com/problems/TALAZY
+
+for T in range(int(input())):
+    N,B,M=map(int,input().split())
+    ans,N=((N+1)//2)*M,N-((N+1)//2)
+    while(N):
+        ans,M=ans+B,M*2
+        ans,N=ans+((N+1)//2)*M,N-((N+1)//2)
+    print(ans)
