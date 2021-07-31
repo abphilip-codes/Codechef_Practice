992dd713f07097e2ce280cf477863b55e16a121c
allenalvin333 <allenalvin333@gmail.com>
2021-07-31 07:48:27 +0530

    programs

--- Beginner/CATSDOGS.py
+++ Beginner/CATSDOGS.py
@@ -2,4 +2,4 @@
 
 for T in range(int(input())):
     c,d,l=map(int,input().split())
-    print("yes") if(l in range(d*4+(max(0,(c-d*2)))*4,d*4+c*4+1)) else print("no")
+    print("yes") if(l in range(d*4+max(0,c-d*2)*4,d*4+c*4+1,4)) else print("no")
