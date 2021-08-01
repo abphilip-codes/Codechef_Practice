268820447d293a14787a578b37f5946162f93528
allenalvin333 <allenalvin333@gmail.com>
2021-08-01 06:44:59 +0530

    programs

--- Beginner/CHEFSQ.py
+++ Beginner/CHEFSQ.py
@@ -1,12 +1,10 @@
 # https://www.codechef.com/problems/CHEFSQ
 
 for T in range(int(input())):
-    N=int(input())
-    n=list(map(int,input().split()))
-    M=int(input())
-    m=list(map(int,input().split()))
-    for i in m:
-        if(i not in n): 
+    N,n,M,m,s=int(input()),list(map(int,input().split())),int(input()),list(map(int,input().split())),0
+    for i in range(M):
+        if(m[i] not in n or n.index(m[i])<s): 
             print("No")
             break
+        s=n.index(m[i])
     else: print("Yes")
