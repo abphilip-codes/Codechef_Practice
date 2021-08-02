283e846b6c86f146ae7bd15eb6e4a04e07f9f465
allenalvin333 <allenalvin333@gmail.com>
2021-08-02 14:39:28 +0530

    programs

---
+++ Beginner/CO92JUDG.py
@@ -0,0 +1,9 @@
+# https://www.codechef.com/problems/CO92JUDG
+
+for T in range(int(input())):
+    n=int(input())
+    A=list(map(int,input().split()))
+    B=list(map(int,input().split()))
+    if((sum(A)-max(A))>(sum(B)-max(B))): print("Alice")
+    elif((sum(A)-max(A))<(sum(B)-max(B))): print("Bob")
+    else: print("Draw")
