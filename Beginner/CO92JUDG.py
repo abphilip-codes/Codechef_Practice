bb0d16a580d10cba592ea350d5667ea025487073
allenalvin333 <allenalvin333@gmail.com>
2021-08-02 14:41:27 +0530

    programs

--- Beginner/CO92JUDG.py
+++ Beginner/CO92JUDG.py
@@ -4,6 +4,6 @@ for T in range(int(input())):
     n=int(input())
     A=list(map(int,input().split()))
     B=list(map(int,input().split()))
-    if((sum(A)-max(A))>(sum(B)-max(B))): print("Alice")
-    elif((sum(A)-max(A))<(sum(B)-max(B))): print("Bob")
+    if((sum(A)-max(A))<(sum(B)-max(B))): print("Alice")
+    elif((sum(A)-max(A))>(sum(B)-max(B))): print("Bob")
     else: print("Draw")
