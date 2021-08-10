81ff7bf7da34d7180d27c51dc88e5ed62f3a9f0c
allenalvin333 <allenalvin333@gmail.com>
2021-08-10 10:32:15 +0530

    programs

--- Beginner/PRGIFT.py
+++ Beginner/PRGIFT.py
@@ -3,10 +3,13 @@
 for T in range(int(input())):
     n,k=map(int,input().split())
     a,c=list(map(int,input().split())),0
-    for i in a:
-        if(i%2==0): c+=1
-        else: c=0
-        if(c==k and k!=0): 
-            print("YES")
-            break
+    if(k in range(1,n+1)):
+        for i in a:
+            if(i%2==0): c+=1
+            else: c=0
+            if(c==k): 
+                print("YES")
+                break
+        else: 
+            print("YES") if(c==k) else print("NO")
     else: print("NO")
