c5566e33ed0b60f2ed1c8bfc6fda4da65df1d294
allenalvin333 <allenalvin333@gmail.com>
2021-08-15 06:32:12 +0530

    programs

--- Beginner/CHRL2.py
+++ Beginner/CHRL2.py
@@ -1,6 +1,12 @@
 # https://www.codechef.com/problems/CHRL2
 
-s,ans=input(),0
-for z in range(len(s)):
-    if(s[z]=='C' and s[z-1]!='C'): ans+=1 
-print(ans)
+s,l=input(),[0]*4
+for z in range(0,len(s)):
+    if(s[z]=='C'): l[0]+=1 
+    elif(s[z]=='H'): 
+        if(l[0]>0): l[0],l[1]=l[0]-1,l[1]+1
+    elif(s[z]=='E'): 
+        if(l[1]>0): l[1],l[2]=l[1]-1,l[2]+1
+    elif(s[z]=='F'): 
+        if(l[2]>0): l[2],l[3]=l[2]-1,l[3]+1
+print(l[-1])
