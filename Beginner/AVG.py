7eb0ccee54e8c925f72b71d0438785b43a5ebd12
allenalvin333 <allenalvin333@gmail.com>
2021-08-16 22:06:54 +0530

    programs

--- Beginner/AVG.py
+++ Beginner/AVG.py
@@ -4,4 +4,4 @@ for T in range(int(input())):
     n,k,v=map(int,input().split())
     a=list(map(int,input().split()))
     ans=v*(n+k)-sum(a)
-    print(int(ans//k)) if(ans%k==0 and ans>=0) else print(-1)
+    print(int(ans/k)) if(ans%k==0 and ans/k>0) else print(-1)
