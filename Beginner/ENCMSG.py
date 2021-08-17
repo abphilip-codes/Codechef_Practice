020258e04f3c2240304f88ce3d38c5d95cbcfb35
allenalvin333 <allenalvin333@gmail.com>
2021-08-17 06:55:47 +0530

    programs

---
+++ Beginner/ENCMSG.py
@@ -0,0 +1,8 @@
+# https://www.codechef.com/problems/ENCMSG
+
+for T in range(int(input())):
+    n,s,st=int(input()),input(),""
+    for i in range(0,n-1,2):
+        print(chr(219-ord(s[i+1])),end="")
+        print(chr(219-ord(s[i])),end="")
+    print(chr(219-ord(s[n-1]))) if(n%2!=0) else print("")
