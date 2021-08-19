4886c294040f1d9933d0a9067329a2f6400a6aaf
allenalvin333 <allenalvin333@gmail.com>
2021-08-19 14:00:35 +0530

    programs

--- Beginner/CODERLIF.py
+++ Beginner/CODERLIF.py
@@ -1,1 +1,12 @@
 # https://www.codechef.com/problems/CODERLIF
+
+for T in range(int(input())):
+    l,c=list(map(int,input().split())),0
+    for i in range(30):
+        if(l[i]==1): 
+            c+=1 
+            if(c>5): 
+                print("#coderlifematters")
+                break
+        else: c=0 
+    else: print("#allcodersarefun")
