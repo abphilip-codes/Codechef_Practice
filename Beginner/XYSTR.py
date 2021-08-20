72050b48132a2bcc9a574161cb73933599f4b580
allenalvin333 <allenalvin333@gmail.com>
2021-08-20 18:46:19 +0530

    programs

---
+++ Beginner/XYSTR.py
@@ -0,0 +1,9 @@
+# https://www.codechef.com/problems/XYSTR
+
+for T in range(int(input())):
+    s,ans,z=input(),0,0
+    while(z<len(s)-1):  
+        if(abs(ord(s[z])-ord(s[z+1]))==1):
+            ans,z=ans+1,i+1
+        z+=1
+    print(ans)
