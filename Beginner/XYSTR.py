e2f963126b78d4cfbd168c84b2af91ac72b3a69c
allenalvin333 <allenalvin333@gmail.com>
2021-08-20 18:49:02 +0530

    programs

--- Beginner/XYSTR.py
+++ Beginner/XYSTR.py
@@ -2,8 +2,7 @@
 
 for T in range(int(input())):
     s,ans,z=input(),0,0
-    while(z<len(s)-1):  
-        if(abs(ord(s[z])-ord(s[z+1]))==1):
-            ans,z=ans+1,z+1
+    while(z<len(s)-1):
+        if(abs(ord(s[z])-ord(s[z+1]))==1): ans,z=ans+1,z+1
         z+=1
     print(ans)
