c115f007dbf8e196de7f8cdff2f42137b44d0c55
allenalvin333 <allenalvin333@gmail.com>
2021-08-21 06:34:29 +0530

    programs

---
+++ Beginner/CHEFSTEP.py
@@ -0,0 +1,7 @@
+# https://www.codechef.com/problems/CHEFSTEP
+
+for T in range(int(input())):
+    n,k=map(int,input().split())
+    l=list(map(int,input().split()))
+    for z in l:
+        print("1",end="") if(z%k==0) else print("0",end="")
