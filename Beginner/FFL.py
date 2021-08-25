ff85fbdc3ebf80bc409202ef41600e52e886090e
allenalvin333 <allenalvin333@gmail.com>
2021-08-25 18:34:48 +0530

    programs

--- Beginner/FFL.py
+++ Beginner/FFL.py
@@ -5,9 +5,8 @@ for T in range(int(input())):
     p,i,l=list(map(int,input().split())),list(map(int,input().split())),[]
     for z in range(n): l.append([i[z],p[z]])
     l.sort()
-    s+=l[0][1]
+    c=0
     for z in range(n):
-        if(l[z][0]==1):
-            s+=l[z][1]
-            break
-    print("yes") if(s<=100) else print("no")
+        if(l[z][0]==0 and c==0): s,c=s+l[z][1],c+1
+        if(l[z][0]==1 and c==1): s,c=s+l[z][1],c+1
+    print("yes") if(s<=100 and c==2) else print("no")
