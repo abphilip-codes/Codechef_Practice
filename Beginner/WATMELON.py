a04e6ca87b69dfae7a0749274d513e42f3bad4eb
allenalvin333 <allenalvin333@gmail.com>
2021-08-27 21:17:09 +0530

    programs

--- Beginner/WATMELON.py
+++ Beginner/WATMELON.py
@@ -2,8 +2,4 @@
 
 for T in range(int(input())):
     n,l=int(input()),list(map(int,input().split()))
-    for z in range(n):
-        if(l[z]%z!=0):
-            print("NO")
-            break
-    else: print("YES")
+    print("YES") if(sum(l)>=0) else print("NO")
