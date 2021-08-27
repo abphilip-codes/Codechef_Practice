d0720e2acd8788ad65354afd2e1bd15d3bcb29b0
allenalvin333 <allenalvin333@gmail.com>
2021-08-27 21:44:25 +0530

    programs

--- Beginner/NW1.py
+++ Beginner/NW1.py
@@ -2,7 +2,6 @@
 
 for T in range(int(input())):
     w,s=map(str,input().split())
-    days=["mon", "tues", "wed", "thurs", "fri", "sat", "sun"]
-    ans=[4]*7
+    days,ans=["mon", "tues", "wed", "thurs", "fri", "sat", "sun"],[4]*7
     for z in range(0,int(w)-28): ans[(z+days.index(s))%7]+=1
     print(*ans)
