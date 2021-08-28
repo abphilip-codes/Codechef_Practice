e62252e2daf2a266ac6f94f453e70628f2292d6d
allenalvin333 <allenalvin333@gmail.com>
2021-08-28 22:23:23 +0530

    programs

--- Beginner/DEVARRAY.py
+++ Beginner/DEVARRAY.py
@@ -1,5 +1,6 @@
 # https://www.codechef.com/problems/DEVARRAY
 
 n,Q=map(int,input().split())
-a=list(map(int,input().split()))
-for z in range(Q): print("Yes") if(int(input()) in range(min(a),max(a)+1)) else print("No")
+max1,min1,a=-99999999999,99999999999,list(map(int,input().split()))
+for z in range(n): min1,max1 = min(min1,a[z]),max(max1,a[z])
+for z in range(Q): print("Yes") if(int(input()) in range(min1,max1+1)) else print("No")
