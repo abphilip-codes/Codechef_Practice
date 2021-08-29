f4c5b8d9760823c3208496ae12ae6537e90e77c3
allenalvin333 <allenalvin333@gmail.com>
2021-08-29 21:49:25 +0530

    programs

--- Beginner/WDTBAM.py
+++ Beginner/WDTBAM.py
@@ -1,7 +1,7 @@
 # https://www.codechef.com/problems/WDTBAM
 
 for T in range(int(input())):
-    n,q,a,w,s=int(input()),input(),input(),list(map(int,input().split())),0
+    n,q,a,w,s=int(input()),input(),input(),list(map(int,input().split())),1
     for z in range(n): 
         if(q[z]==a[z]): s+=1
-    print(w[s])
+    print(max(w[:s]))
