3a952f75fbf474014b4c0a5ca1301177d165263a
allenalvin333 <allenalvin333@gmail.com>
2021-09-05 12:47:46 +0530

    programs

--- Beginner/FBMT.py
+++ Beginner/FBMT.py
@@ -4,9 +4,11 @@ for _ in range(int(input())):
     s=[]
     for n in range(int(input())): s.append(input())
     st=list(set(s))
-    if(len(st)==1): print(st[0])
-    else:
-        a,b=s.count(st[0]),s.count(st[1])
-        if(a>b): print(st[0])
-        elif(a<b): print(st[1])
-        else: print("Draw")
+    try:
+        if(len(st)==1): print(st[0])
+        else:
+            a,b=s.count(st[0]),s.count(st[1])
+            if(a>b): print(st[0])
+            elif(a<b): print(st[1])
+            else: print("Draw")
+    except: print("Draw")
