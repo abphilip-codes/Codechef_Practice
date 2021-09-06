c837e359b0bb8f2f8265e1b9c66473ac7995ba6a
allenalvin333 <allenalvin333@gmail.com>
2021-09-06 08:25:15 +0530

    programs

--- Beginner/HMAPPY2.py
+++ Beginner/HMAPPY2.py
@@ -1,9 +1,7 @@
 # https://www.codechef.com/problems/HMAPPY2
 
-def gcd(x,y):
-   while(y): x,y=y,x%y
-   return x
 for _ in range(int(input())):
     n,a,b,k=map(int,input().split())
-    lcm=a*b/gcd(a,b)
-    print("Lose") if(k>(n//a+n//b-2*(n//int(lcm)))) else print("Win")
+    x,y=a,b
+    while(y): x,y=y,x%y
+    print("Lose") if(k>(n//a+n//b-2*(n//((a*b)//x)))) else print("Win")
