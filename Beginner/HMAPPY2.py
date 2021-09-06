b6b07e6d0a7c7a171ae6dbba6fd06606945f5463
allenalvin333 <allenalvin333@gmail.com>
2021-09-06 08:13:26 +0530

    programs

--- Beginner/HMAPPY2.py
+++ Beginner/HMAPPY2.py
@@ -1,2 +1,8 @@
 # https://www.codechef.com/problems/HMAPPY2
 
+for _ in range(int(input())):
+    n,a,b,k=map(int,input().split())
+    for z in range(1,n+1):
+        if(z%a==0 or z%b==0): 
+            if not (z%a==0 and z%b==0): k-=1
+    print("Lose") if(k) else print("Win")
