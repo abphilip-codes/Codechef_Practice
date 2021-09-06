428decd746137e0d80b9e99f4c883f9fac2d8836
allenalvin333 <allenalvin333@gmail.com>
2021-09-06 08:39:23 +0530

    programs

--- Beginner/BIGSALE.py
+++ Beginner/BIGSALE.py
@@ -1,2 +1,8 @@
 # https://www.codechef.com/problems/BIGSALE
 
+for _ in range(int(input())):
+    ans=0.0
+    for n in range(int(input())):
+        p,q,d=map(int,input().split())
+        ans+=((p*q)-(p*(((100+d)/100)*((100-d)/100))*q))
+    print(ans)
