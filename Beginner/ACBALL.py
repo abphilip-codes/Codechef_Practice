78927ed1734dfa90ba7e6369018e5f1d74248957
allenalvin333 <allenalvin333@gmail.com>
2021-09-07 07:41:57 +0530

    programs

--- Beginner/ACBALL.py
+++ Beginner/ACBALL.py
@@ -1,2 +1,10 @@
 # https://www.codechef.com/problems/ACBALL
 
+for _ in range(int(input())):
+    a,b,k=input(),input(),""
+    for z in range(len(a)):
+        if(a[z]==b[z]):
+            if(a[z]=='W'): k+='B'
+            else: k+='W'
+        else: k+='B'
+    print(k)
