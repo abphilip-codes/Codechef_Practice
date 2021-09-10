3338d4af379232e4e695a77861c2f206b7f002de
allenalvin333 <allenalvin333@gmail.com>
2021-09-10 09:19:15 +0530

    programs

--- Beginner/CHEFWORK.py
+++ Beginner/CHEFWORK.py
@@ -1,2 +1,5 @@
 # https://www.codechef.com/problems/CHEFWORK
 
+n,c,t=int(input()),list(map(int,input().split())),list(map(int,input().split()))
+a,b = [x for y,x in sorted(zip(t,c))],[y for y,x in sorted(zip(t,c))]
+print(min(a[b.index(1)]+a[b.index(2)],a[b.index(3)]))
