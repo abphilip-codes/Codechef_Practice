73cb479b253767103d38796aa7f015b5a61c9da8
allenalvin333 <allenalvin333@gmail.com>
2021-09-10 10:13:32 +0530

    programs

--- Beginner/LCOLLIS.py
+++ Beginner/LCOLLIS.py
@@ -3,6 +3,8 @@
 import math
 def ncr(n,r): return math.factorial(n)//math.factorial(r)//math.factorial(n-r) if(n>=r) else 0
 for _ in range(int(input())):
-    (n,m),ans = map(int,input().split()),0
-    for z in range(n): ans+=ncr(input().count('1'),2)
+    (n,m),ans,a = map(int,input().split()),0,[]
+    for z in range(n): a.append(input())
+    a = [[a[y][x] for y in range(len(a))] for x in range(len(a[0]))]
+    for z in range(m): ans+=ncr(a[z].count('1'),2)
     print(ans)
