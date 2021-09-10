3e391d177a83a5885a63c93232b12acaaae1d090
allenalvin333 <allenalvin333@gmail.com>
2021-09-10 09:25:32 +0530

    programs

--- Beginner/CHEFWORK.py
+++ Beginner/CHEFWORK.py
@@ -2,4 +2,5 @@
 
 n,c,t=int(input()),list(map(int,input().split())),list(map(int,input().split()))
 a,b = [x for y,x in sorted(zip(t,c))],[y for y,x in sorted(zip(t,c))]
-print(min(a[b.index(1)]+a[b.index(2)],a[b.index(3)]))
+if(1 in b and 2 in b and 3 in b): print(min(a[b.index(1)]+a[b.index(2)],a[b.index(3)]))
+else: print(a[b.index(1)]+a[b.index(2)]) if(3 not in b) else print(a[b.index(3)])
