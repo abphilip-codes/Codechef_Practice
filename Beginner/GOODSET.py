c7a6c7f23d90a1cfc19ccb624116a0ddb147daa2
allenalvin333 <allenalvin333@gmail.com>
2021-09-10 07:14:04 +0530

    programs

---
+++ Beginner/GOODSET.py
@@ -0,0 +1,5 @@
+# https://www.codechef.com/problems/GOODSET
+
+for _ in range(int(input())):
+    for z in range(1,int(input())*2,2): print(z, end=' ')
+    print()         # Sum of two odd numbers is always even
