19d32bad9e6d6af659a856699fb806ea050ba6d9
allenalvin333 <allenalvin333@gmail.com>
2021-09-10 10:04:03 +0530

    programs

--- Beginner/LCOLLIS.py
+++ Beginner/LCOLLIS.py
@@ -1,1 +1,8 @@
 # https://www.codechef.com/problems/LCOLLIS
+
+import math
+def ncr(n,r): return math.factorial(n)//math.factorial(r)//math.factorial(n-r) if(n>=r) else 0
+for _ in range(int(input())):
+    (n,m),ans = map(int,input().split()),0
+    for z in range(n): ans+=ncr(input().count('1'),2)
+    print(ans)
