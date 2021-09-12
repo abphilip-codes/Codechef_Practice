21ed47fcd889d399bcf9374c5c1ccf3d6bc4a47d
allenalvin333 <allenalvin333@gmail.com>
2021-09-12 20:20:06 +0530

    programs

--- Beginner/CHEFDETE.py
+++ Beginner/CHEFDETE.py
@@ -1,2 +1,7 @@
 # https://www.codechef.com/submit/CHEFDETE
 
+n,l=int(input()),list(map(int,input().split()))
+a=[0]*n
+for z in range(n): a[l[z]]=1
+for z in range(1,n+1): 
+    if(a[z-1]==0): print(z)     
