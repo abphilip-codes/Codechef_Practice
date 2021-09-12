4a5ce1fa05cef0f14a603b46b61ef75055d05e31
allenalvin333 <allenalvin333@gmail.com>
2021-09-12 20:24:05 +0530

    programs

--- Beginner/CHEFDETE.py
+++ Beginner/CHEFDETE.py
@@ -1,7 +1,8 @@
 # https://www.codechef.com/submit/CHEFDETE
 
 n,l=int(input()),list(map(int,input().split()))
-a=[0]*n
+a=[0]*(n+1)
 for z in range(n): a[l[z]]=1
 for z in range(1,n+1): 
-    if(a[z-1]==0): print(z)     
+    if(a[z]==0): print(z,end=" ")     
+print()
