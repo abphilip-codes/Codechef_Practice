a80f6829219bf62afb7dfd6700c7802d3b0cf961
allenalvin333 <allenalvin333@gmail.com>
2021-09-12 20:20:32 +0530

    programs

--- Beginner/SNELECT.py
+++ Beginner/SNELECT.py
@@ -1,2 +1,13 @@
 # https://www.codechef.com/problems/SNELECT
 
+for _ in range(int(input())):
+    st,a,z = "k"+input()+"k",[],1
+    a = [f for f in st]
+    while(a[z]!='k'): 
+        if(a[z-1]=='s'): 
+            a.pop(z-1)
+            z-=1
+        elif(a[z+1]=='s'): 
+            a.pop(z+1)
+        z+=1 
+    print(a)
