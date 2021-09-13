608b5a29ac4618517842a235adfd7f6f3c4f66fb
allenalvin333 <allenalvin333@gmail.com>
2021-09-13 08:18:51 +0530

    programs

--- Beginner/CFMM.py
+++ Beginner/CFMM.py
@@ -1,2 +1,6 @@
 # https://www.codechef.com/problems/CFMM
 
+for _ in range(int(input())): 
+    s=""
+    for n in range(int(input())): s+=input()
+    print(min(s.count('c')//2,s.count('e')//2,s.count('o'),s.count('d'),s.count('h'),s.count('f')))
