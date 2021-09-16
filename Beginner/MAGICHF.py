b72b4ad98cad1eb2bfdc8ea129310fb5300829c5
allenalvin333 <allenalvin333@gmail.com>
2021-09-16 20:11:45 +0530

    programs

--- Beginner/MAGICHF.py
+++ Beginner/MAGICHF.py
@@ -1,1 +1,9 @@
 # https://www.codechef.com/problems/MAGICHF
+
+for _ in range(int(input())):
+    n,x,s=map(int,input().split())
+    for z in range(s):
+        a,b=map(int,input().split())
+        if(a==x): x=b 
+        elif(b==x): x=a
+    print(x)
