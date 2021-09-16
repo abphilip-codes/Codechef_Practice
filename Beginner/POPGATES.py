7ec822d4d7bdbf5a48e70e021454824d598aa914
allenalvin333 <allenalvin333@gmail.com>
2021-09-16 11:28:04 +0530

    programs

--- Beginner/POPGATES.py
+++ Beginner/POPGATES.py
@@ -2,10 +2,10 @@
 
 for _ in range(int(input())):
     n,k=map(int,input().split())
-    l=list(map(int,input().split()))
+    l=list(input().split())
     for z in range(n-1,n-k-1,-1):
         if(l[z]=='H'):
-            for y in range(z): l[y]='T' if(l[y]=='H') else l[y]='H'
-                # if(l[y]=='H'): l[y]='T'
-                # else: l[y]=
-    print(l)
+            for y in range(z): 
+                if(l[y]=='H'): l[y]='T'
+                else: l[y]='H'
+    print(l[:n-k].count('H'))
