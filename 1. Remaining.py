0211fd994ea909e104d5559cd1f4034c0f075170
allenalvin333 <allenalvin333@gmail.com>
2021-09-20 08:17:55 +0530

    programs

---
+++ 1. Remaining.py
@@ -0,0 +1,17 @@
+# https://www.codechef.com/problems/PRGIFT
+# https://www.codechef.com/problems/QUADROOT
+# https://www.codechef.com/problems/TEMPLELA
+# https://www.codechef.com/problems/BUGCAL
+# https://www.codechef.com/problems/PALIN
+# https://www.codechef.com/problems/WDTBAM
+# https://www.codechef.com/problems/CHEFNWRK
+# https://www.codechef.com/problems/LIFELTD
+# https://www.codechef.com/problems/CHFGCD
+# https://www.codechef.com/problems/CHFPLN
+# https://www.codechef.com/problems/MODEQUAL
+# https://www.codechef.com/problems/ARRT
+# https://www.codechef.com/problems/ETUP
+# https://www.codechef.com/problems/MULTHREE
+# https://www.codechef.com/problems/XXOORR
+# https://www.codechef.com/problems/MINNOTES
+# https://www.codechef.com/problems/THREEPTS
