1a8ea8a8a3bbd7404b331833970ebd6c7575ab59
allenalvin333 <allenalvin333@gmail.com>
2021-12-10 10:22:36 +0530

    programs

--- BUGCAL.py
+++ BUGCAL.py
@@ -1,6 +1,7 @@
 # https://www.codechef.com/problems/BUGCAL
 
 for T in range(int(input())):
-    l=input().split()
-    z="0"*(len(max(l))-len(min(l)))
-    print(z)
+    l=list(map(int,input().split()))
+    a,b,s=str(max(l)),"0"*(len(str(max(l)))-len(str(min(l))))+str(min(l)),""
+    for z in range(len(a)): s+=str((int(a[z])+int(b[z]))%10)
+    print(s)
