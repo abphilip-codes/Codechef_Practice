64151ee22fea004db7531b7f8e643623563c885d
allenalvin333 <allenalvin333@gmail.com>
2021-12-21 12:51:06 +0530

    programs

--- WDTBAM.py
+++ WDTBAM.py
@@ -5,3 +5,14 @@ for T in range(int(input())):
     for z in range(n): 
         if(q[z]==a[z]): s+=1
     print(max(w[:s]))
+
+for T in range(int(input())):
+    n,c,a,p,s=int(input()),input(),input(),list(map(int,input().split())),0
+    for z in range(len(c)):
+        if(a[z]==c[z]): s+=1
+    if(s==n): print(p[n])
+    else:
+        ans=0
+        for z in range(s):
+            ans=max(ans,p[z])
+        print(ans)
